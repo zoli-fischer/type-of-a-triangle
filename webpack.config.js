31f06f548696af7630428232ca6c1a7fd723d735
Zoltan Fischer <zoli_fischer@yahoo.com>
2018-12-08 11:58:45 +0100

    added npm

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+console.log('WEBPACK_ENV: ', process.env.WEBPACK_ENV);
+console.log('WEBPACK_POLL: ', process.env.WEBPACK_POLL ? 'true' : 'false');
+
+module.exports = {
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: './js/index.js',
+    },
+    entry: path.resolve(__dirname, 'src/index.js'),
+    plugins: [
+        new CleanWebpackPlugin(['build']),
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public/index.html') }),
+    ],
+    mode: 'development',
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.js', '.json', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader',
+                ],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader'],
+            },
+        ],
+    },
+};
